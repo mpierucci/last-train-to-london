6db68a65a35a0d10a7739f176b8f00378c6d0d27
Marco <marco1pierucci@gmail.com>
2019-10-31 20:00:55 +0000

    add flow use case

--- usecase/src/main/kotlin/com/mpierucci/lastratintolondon/usecase/coroutine/usecases.kt
+++ usecase/src/main/kotlin/com/mpierucci/lastratintolondon/usecase/coroutine/usecases.kt
@@ -2,6 +2,7 @@ package com.mpierucci.lastratintolondon.usecase.coroutine
 
 import com.mpierucci.lastratintolondon.usecase.Either
 import com.mpierucci.lastratintolondon.usecase.failure.Failure
+import kotlinx.coroutines.flow.Flow
 
 interface SuspendedUseCase<REQUEST, RESULT> {
     suspend fun execute(request: REQUEST): Either<Failure, RESULT>
@@ -9,4 +10,12 @@ interface SuspendedUseCase<REQUEST, RESULT> {
 
 interface SuspendedNoArgUseCase<RESULT> {
     suspend fun execute(): Either<Failure, RESULT>
+}
+
+interface FlowUseCase<REQUEST, RESULT> {
+    suspend fun execute(request: REQUEST): Flow<Either<Failure, RESULT>>
+}
+
+interface FlowNoArgUseCase<RESULT> {
+    suspend fun execute(): Flow<Either<Failure, RESULT>>
 }
