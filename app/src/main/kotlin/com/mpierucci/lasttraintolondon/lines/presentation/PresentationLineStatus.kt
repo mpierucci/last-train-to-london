5c7851d7afeacc0a18a06bbbd1a8d16d9ccd8ee4
Marco <marco1pierucci@gmail.com>
2019-11-04 01:12:47 +0000

    apply status colors

--- app/src/main/kotlin/com/mpierucci/lasttraintolondon/lines/presentation/PresentationLineStatus.kt
+++ app/src/main/kotlin/com/mpierucci/lasttraintolondon/lines/presentation/PresentationLineStatus.kt
@@ -1,9 +1,11 @@
 package com.mpierucci.lasttraintolondon.lines.presentation
 
+import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 
 data class PresentationLineStatus(
     @DrawableRes val badgeId: Int,
+    @ColorRes val statusColor: Int,
     val disruptionVisibility: Int,
     val name: String,
     val status: String
