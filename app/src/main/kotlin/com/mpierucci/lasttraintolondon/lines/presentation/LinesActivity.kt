33a57b5626e94e669c3eeb3b78c1ec343bd28cae
Marco <marco1pierucci@gmail.com>
2019-11-15 16:54:46 +0000

    basic bottom nav navigation

--- app/src/main/kotlin/com/mpierucci/lasttraintolondon/lines/presentation/LinesActivity.kt
+++ app/src/main/kotlin/com/mpierucci/lasttraintolondon/lines/presentation/LinesActivity.kt
@@ -2,6 +2,8 @@ package com.mpierucci.lasttraintolondon.lines.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
 import com.mpierucci.lasttraintolondon.R
 import com.mpierucci.lasttraintolondon.core.fragment.GenericFragmentFactory
 import com.mpierucci.lasttraintolondon.di.injector
@@ -19,17 +21,11 @@ class LinesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lines)
         setSupportActionBar(toolbar)
-        /*
-        if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                val fragment = supportFragmentManager.fragmentFactory.instantiate(
-                    classLoader,
-                    LineStatusFragment::class.java.canonicalName.orEmpty()
-                )
-                add(R.id.lineFragmentContainer, fragment)
-            }
-        }
-        */
+        val navigationController = Navigation.findNavController(
+            this,
+            R.id.linesNavigationHost
+        )
 
+        linesBottomNavigation.setupWithNavController(navigationController)
     }
 }
