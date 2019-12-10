261d3eee8b4ec29e4880612ddc09a38ba65954b6
Marco <marco1pierucci@gmail.com>
2019-12-10 16:05:15 +0000

    fix view contract loading

--- core/src/main/kotlin/com/mpierucci/lasttraintolondon/core/presentation/ViewContract.kt
+++ core/src/main/kotlin/com/mpierucci/lasttraintolondon/core/presentation/ViewContract.kt
@@ -5,5 +5,5 @@ import com.mpierucci.lastratintolondon.usecase.failure.Failure
 sealed class ViewContract<out T> {
     data class Success<T>(val result: T) : ViewContract<T>()
     data class Error(val failure: Failure) : ViewContract<Nothing>()
-    data class Loading<T>(val loading: Boolean) : ViewContract<Nothing>()
+    data class Loading(val loading: Boolean) : ViewContract<Nothing>()
 }
