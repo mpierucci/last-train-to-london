d783bc97e8a613b13c21769fafe0a59d23e032f3
Marco <marco1pierucci@gmail.com>
2019-12-17 17:47:43 +0000

    remove github authentication

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import java.io.FileInputStream
-import java.util.*
 
 plugins {
     kotlin("jvm") version Libs.kotlinVersion
@@ -20,12 +18,6 @@ buildscript {
 
 
 allprojects {
-    val githubProperties by lazy {
-        Properties().apply {
-            load(FileInputStream(rootProject.file("github.properties")))
-        }
-    }
-
     repositories {
         google()
         jcenter()
@@ -33,10 +25,6 @@ allprojects {
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/mpierucci/Android-Architecture")
-            credentials {
-                username = System.getenv("GPR_USER") ?: githubProperties.getProperty("gpr.usr")
-                password = System.getenv("GPR_API_KEY") ?: githubProperties.getProperty("gpr.key")
-            }
         }
     }
     apply(file("$rootDir/qa/detekt/detekt.gradle"))
