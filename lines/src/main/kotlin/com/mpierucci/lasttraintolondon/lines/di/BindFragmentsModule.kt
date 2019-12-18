4ae847fd8d262e8c0dbf47de6bed19f64448f533
Marco <marco1pierucci@gmail.com>
2019-12-18 19:24:32 +0000

    Fix LineStatus issues: - Rename fragment to screen, as in SA architecture each fragment should be a screen

--- lines/src/main/kotlin/com/mpierucci/lasttraintolondon/lines/di/BindFragmentsModule.kt
+++ lines/src/main/kotlin/com/mpierucci/lasttraintolondon/lines/di/BindFragmentsModule.kt
@@ -2,7 +2,7 @@ package com.mpierucci.lasttraintolondon.lines.di
 
 import androidx.fragment.app.Fragment
 import com.mpierucci.lasttraintolondon.core.fragment.FragmentClassKey
-import com.mpierucci.lasttraintolondon.lines.presentation.LineStatusFragment
+import com.mpierucci.lasttraintolondon.lines.presentation.LineStatusScreen
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -12,6 +12,6 @@ abstract class BindFragmentsModule {
 
     @Binds
     @IntoMap
-    @FragmentClassKey(LineStatusFragment::class)
-    abstract fun bindLineStatusFragment(fragment: LineStatusFragment): Fragment
+    @FragmentClassKey(LineStatusScreen::class)
+    abstract fun bindLineStatusFragment(screen: LineStatusScreen): Fragment
 }
