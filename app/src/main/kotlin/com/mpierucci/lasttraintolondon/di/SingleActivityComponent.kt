4a395435004ad39b9ec34af85770a26730199cc5
Marco <marco1pierucci@gmail.com>
2020-02-20 20:04:39 +0000

    Refactor DI in a cleaner way

--- app/src/main/kotlin/com/mpierucci/lasttraintolondon/di/SingleActivityComponent.kt
+++ app/src/main/kotlin/com/mpierucci/lasttraintolondon/di/SingleActivityComponent.kt
@@ -1,9 +1,12 @@
 package com.mpierucci.lasttraintolondon.di
 
+import com.mpierucci.lasttraintolondon.lines.di.LineStatusesModule
 import com.mpierucci.lasttraintolondon.ui.SingleActivity
 import dagger.Subcomponent
 
-@Subcomponent
+@Subcomponent(
+    modules = [LineStatusesModule::class, BindFragmentsModule::class]
+)
 interface SingleActivityComponent {
     fun inject(activity: SingleActivity)
 }
--- app/src/main/kotlin/com/mpierucci/lasttraintolondon/ui/SingleActivity.kt
+++ app/src/main/kotlin/com/mpierucci/lasttraintolondon/ui/SingleActivity.kt
@@ -1,6 +1,5 @@
 package com.mpierucci.lasttraintolondon.ui
 
-import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
